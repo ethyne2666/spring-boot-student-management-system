3065f79a02a9b8ef2b2ccbd6639f0eab46ae1411
temp <temporary2666@gmail.com>
2026-06-12 21:43:05 +0530

    add new student feature

--- src/main/java/dev/nullLogic/student_management_system/controller/StudentController.java
+++ src/main/java/dev/nullLogic/student_management_system/controller/StudentController.java
@@ -5,6 +5,8 @@ import dev.nullLogic.student_management_system.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,6 +26,24 @@ public class StudentController {
         List<StudentDto> students = studentService.getAllStudents();
         model.addAttribute("students" , students);
         return "students";
+    }
+
+
+    //handler method to handle new student request
+
+    @GetMapping("/students/new")
+    public String newStudent(Model model){
+        // student model object to store student form data
+        StudentDto studentDto = new StudentDto();
+        model.addAttribute("student",studentDto);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    //handler method to handle save student form submit request
+    public String saveStudent(@ModelAttribute("student") StudentDto student){
+        studentService.createStudent(student);
+        return "redirect:/students";
     }
 
 
